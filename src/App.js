2e2b07489bdf0b5c23c2691a2a4ba77d4f13a37b
Machinix <63652093+Machinix@users.noreply.github.com>
2020-11-06 17:49:52 +0500

    Add Parcel, Extract pet out
    Added Parcel
    seperated components in different files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-const pet = (name, type) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, type),
-  ]);
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './Pet';
 
 const App = () => {
-  return React.createElement('div', { id: 'something' }, [
-    React.createElement('h1', {}, 'Adopt Me'),
-    React.createElement(pet, { name: 'Pyari', type: 'doberman' }),
-    React.createElement(pet, { name: 'Fazloo', type: 'gorilla' }),
-    React.createElement(pet, {
-      name: 'Subhani',
-      type: 'life jacket',
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel',
+    }),
+    React.createElement(Pet, {
+      name: 'Doink',
+      animal: 'Cat',
+      breed: 'Mix',
     }),
   ]);
 };
+
 ReactDOM.render(
   React.createElement(App),
   document.getElementById('root'),
