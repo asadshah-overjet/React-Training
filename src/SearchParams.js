2aa38ed1389fe314a37029fb5a84fcec1d9a263c
Machinix <63652093+Machinix@users.noreply.github.com>
2020-11-10 18:53:08 +0500

    Use Effect added
    Asynchronous call to api added using React hooks. Got the data and populated based on some dependencies

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './UseDropdown';
 
 const SearchParam = () => {
   const [location, setLocation] = useState('Seattle, WA');
-  const [breeds, steBreeds] = useState([]);
+  const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown(
     'Animal',
     'dog',
     ANIMALS,
   );
-  const [breed, BreedDropDown] = useDropdown('Breed', '', breeds);
+  const [breed, BreedDropDown, setBreed] = useDropdown(
+    'Breed',
+    '',
+    breeds,
+  );
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed('');
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedString = breeds.map(({ name }) => name);
+      setBreeds(breedString);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
